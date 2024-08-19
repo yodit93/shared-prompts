433ad43d58bab73da0c289ffcf5e04144cef83f4
yodit93 <yoditabebe9391@gmail.com>
2024-08-19 15:42:24 +0300

    Add layout to subfolder to add common layouts between pages in that specific folder

---
+++ app/posts/layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// This page layout component is shared with in all pages within the posts directory. It is used to add a common layout to all pages within the posts directory. Create a new file at app/posts/layout.js and add the following code:
+
+const PageLayout = () => {
+  return (
+    <div>
+      Navigate to top
+    </div>
+  )
+}
+
+export default PageLayout
