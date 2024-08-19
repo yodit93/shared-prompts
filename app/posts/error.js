09a931dc278dbf3ac0f39dd23088739dd002c7e4
yodit93 <yoditabebe9391@gmail.com>
2024-08-19 15:46:44 +0300

    Use loading and error next special pages

---
+++ app/posts/error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// This is the error page. It will be rendered when there is an error in the app.
+
+const error = () => {
+  return (
+    <div>
+      Error
+    </div>
+  )
+}
+
+export default error
---
+++ app/posts/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// This page will be rendered until the pages are loaded.
+
+const loading = () => {
+  return (
+    <div>
+      Loading....
+    </div>
+  )
+}
+
+export default loading
