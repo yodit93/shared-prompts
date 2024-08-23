9866c120989996b45f9ffe610d8380b6642d3df1
yodit93 <yoditabebe9391@gmail.com>
2024-08-23 18:20:34 +0300

    Create provider component to access users session using sessionprovider comp from next-auth

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { SessionProvider } from "next-auth/react"
 
-const Provider = () => {
+const Provider = ( {children, session} ) => {
   return (
-    <div>
-      
-    </div>
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
   )
 }
 
