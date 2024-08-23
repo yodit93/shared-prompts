da95f87d471b922f0031026807badafd1ef6e615
yodit93 <yoditabebe9391@gmail.com>
2024-08-23 10:46:07 +0300

    use signout method from next-auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,9 @@
 import Link  from 'next/link'
 import Image from 'next/image'
+import { signout } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = false ;
+  const isUserLoggedIn = true;
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
@@ -14,16 +15,28 @@ const Nav = () => {
         />
         <p className='logo_text'>Promptopia</p>
       </Link>
+      {/* disktop navigation */}
       <div className='sm:flex hidden'>
         {isUserLoggedIn ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Prompt
             </Link>
+
+            <button type='button' onClick={signout} className='outline_btn'>Sign Out</button>
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='Profile Image'
+              />
+            </Link>
           </div>
         ) : (
           <>
-
+            
           </>
         )}
       </div>
