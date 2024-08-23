52aebb9b80102f8605ded284563805d74221bc82
yodit93 <yoditabebe9391@gmail.com>
2024-08-23 11:28:54 +0300

    Use getProvider and signIn from next-auth to sign in with providers

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,21 @@
+'use client'
+
 import Link  from 'next/link'
 import Image from 'next/image'
-import { signout } from 'next-auth/react'
+import { signOut, signIn, getProviders } from 'next-auth/react'
+import { useState, useEffect } from 'react'
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+  const [providers, setProviders] = useState(null)
+
+  useEffect(() => {
+    const fetchProviders= async () => {
+      const providers = await getProviders()
+      setProviders(providers)
+    }
+    fetchProviders()
+  }, [])
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
@@ -23,7 +35,7 @@ const Nav = () => {
               Create Prompt
             </Link>
 
-            <button type='button' onClick={signout} className='outline_btn'>Sign Out</button>
+            <button type='button' onClick={signOut} className='outline_btn'>Sign Out</button>
             <Link href='/profile'>
               <Image
                 src='/assets/images/logo.svg'
@@ -36,7 +48,17 @@ const Nav = () => {
           </div>
         ) : (
           <>
-            
+            {/* <button type='button' onClick={signIn}>Sign In</button> */}
+            {providers && Object.values(providers).map((provider) => (
+              <button 
+                type='button' 
+                key={provider.name} 
+                onClick={() => signIn(provider.id)}
+              >
+                
+                Sign in with {provider.name}
+              </button>
+            ))}
           </>
         )}
       </div>
